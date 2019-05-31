a43872279d6d609b08d762bb84df24e074370720
Zmaplex <Zmaplex@gmail.com>
2019-05-31 15:49:41 +0800

    迁移至AndroidX，并更新相关组件到最新版本

--- app/src/main/java/com/qingmei2/samplejetpack/MainActivity.kt
+++ app/src/main/java/com/qingmei2/samplejetpack/MainActivity.kt
@@ -2,7 +2,7 @@ package com.qingmei2.samplejetpack
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
